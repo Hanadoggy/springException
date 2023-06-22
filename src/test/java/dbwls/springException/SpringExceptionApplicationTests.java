425364e1ea4a53b12005538f9ed393b0a36fbb3f
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-06-22 19:53:00 +0900

    Initial commit

---
+++ src/test/java/dbwls/springException/SpringExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package dbwls.springException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
