448f43d213a05d992106d11d88e13d4cdd7fd0e1
Hanadoggy <78478981+Hanadoggy@users.noreply.github.com>
2023-06-22 22:01:24 +0900

    add exception
    예외 페이지 처리 - filter, interceptor 사용 그리고 스프링 부트

---
+++ src/main/java/dbwls/springException/WebConfig.java
@@ -0,0 +1,36 @@
+package dbwls.springException;
+
+import dbwls.springException.filter.LogFilter;
+import dbwls.springException.interceptor.LogInterceptor;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**");
+    }
+
+    @Bean
+    public FilterRegistrationBean logFilter() {
+
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST);
+        // DispatcherType.ERROR 를 포함하면 ERROR 일 경우에도 필터 적용, REQUEST 는 기본 값
+
+        return filterRegistrationBean;
+    }
+}
